7b912207e0ca8d9dbbc11abe396c031d7472d0da
Kriegsmarine13 <rediska195@mail.ru>
2024-09-22 22:36:18 +0300

    updates

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require("axios")
 const app = express()
-const port = 80
+const port = process.env.PORT || 8080
 
 app.set('trust proxy', true);
 app.use(bodyParser.json())
