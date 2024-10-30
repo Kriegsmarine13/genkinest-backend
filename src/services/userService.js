f74a810213b03df4937794229b888d4e47f8d3dc
Kriegsmarine13 <rediska195@mail.ru>
2024-10-31 03:48:46 +0900

    fix demo user creation

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ const userModel = require("../models/user")
 
 async function addUserToFamily(organizationId, usersArr) {
     let organization = await organizationModel.getOrganization(organizationId);
-    await userModel.updateUser({customFields: {familyId: organizationId}})
+    await userModel.updateUser(usersArr, {customFields: {familyId: organizationId}})
     return organizationModel.updateOrganization(organizationId, {
         users: organization.users.length > 1 ? organization.users.concat(usersArr.filter((item) => organization.users.indexOf(item) < 0)) : usersArr
     })
